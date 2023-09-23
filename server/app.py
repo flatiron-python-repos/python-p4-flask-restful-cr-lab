0e97c2cee457884461325b30eaf78b87e442dcc2
George-Okumu <gokumu12@gmail.com>
2023-09-23 03:00:05 +0300

    done

--- server/app.py
+++ server/app.py
@@ -17,11 +17,42 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        # plants = [plant for plant in ]
+        return make_response(jsonify([{
+            "id": plants.id,
+            "name": plants.name
+        }for plants in Plant.query.all()]), 200)
+    
+    def post(self):
+        da = request.get_json()
+        new_plant = Plant(
+            name = da['name'],
+            image = da['image'],
+            price = da['price']
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        return make_response(new_plant.to_dict(), 201)
+        
+api.add_resource(Plants, "/plants")
+
 
 class PlantByID(Resource):
-    pass
-        
+
+    def get(self, id):
+        # to_dict() comes from serializemixin
+        plant = Plant.query.filter_by(id = id).first_or_404().to_dict()
+
+        return make_response(
+            jsonify(plant), 200
+        )
+
+    
+api.add_resource(PlantByID, "/plants/<int:id>")
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
